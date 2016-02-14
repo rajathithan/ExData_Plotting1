22e084da1e3bacf1d353b525713aee1f2cd92c7b
rajathithan <rajathithan@gmail.com>
2016-02-14 18:55:33 +0530

    Final Commit - 2
    Final Commit - 2

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-data <- "ec/newecdata.rds"
+summdata <- "ec/newecdata.rds"
 ecdata <- readRDS(data)
 png("ec/plot1.png", width = 480, height = 480)
 hist(ecdata$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "red")
